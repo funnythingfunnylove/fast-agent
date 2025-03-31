c14e0a709c549b665fc5d9de32741e0d03554ce2
fenglei <jz47321@wisdri.com>
2025-03-31 17:30:48 +0800

    change the test

--- agent.py
+++ agent.py
@@ -3,12 +3,13 @@ import asyncio
 from mcp_agent.core.fastagent import FastAgent
 
 # Create the application
-fast = FastAgent("FastAgent Example")
+fast = FastAgent("MCP agents")
 
 
 # Define the agent
-@fast.agent(instruction="You are a helpful AI Agent", servers=["fetch", "filesystem"])
-@fast.agent(name="web", instruction="You are a web agent", servers=["webbrowser"])
+# @fast.agent(instruction="You are a helpful AI Agent", servers=["fetch", "filesystem"])
+# @fast.agent(name="web", instruction="You are a web agent", servers=["webbrowser"])
+@fast.agent(name="v2ex", instruction="You 是 一个 V2ex.com 网站的服务器机器人,", servers=["v2ex"])
 async def main():
     # use the --model command line switch or agent arguments to change model
     async with fast.run() as agent:
